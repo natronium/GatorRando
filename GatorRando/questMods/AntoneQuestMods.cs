ba1302fa33608db89ef74eb519287c6b15e74b63
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2024-06-22 12:39:22 -0500

    Antone quest mods updated for GetByPath

--- GatorRando/questMods/AntoneQuestMods.cs
+++ GatorRando/questMods/AntoneQuestMods.cs
@@ -7,14 +7,12 @@ static class AntoneQuestMods
     public static void Edits()
     {
         //Edits to Antone's Quest
-        GameObject prep_quest = GameObject.Find("Prep Quest");
-        Transform prep_subquests = prep_quest.transform.Find("Subquests");
-        GameObject entomologist_quest = prep_subquests.Find("Entomologist").gameObject;
+        GameObject entomologist_quest = Util.GetByPath("West (Forest)/Prep Quest/Subquests/Entomologist");
         QuestStates entomologist_quest_qs = entomologist_quest.GetComponent<QuestStates>();
-        GameObject sneak_seq = entomologist_quest.transform.Find("Sneak up sequence").gameObject;
+        GameObject sneak_seq = Util.GetByPath("West (Forest)/Prep Quest/Subquests/Entomologist/Sneak up sequence");
         entomologist_quest_qs.states[1].stateObjects = entomologist_quest_qs.states[1].stateObjects.Remove(sneak_seq);
         sneak_seq.SetActive(false);
-        if (ArchipelagoManager.ItemIsUnlocked("Bug Net (Sword)"))
+        if (ArchipelagoManager.ItemIsUnlocked("Sword_Net"))
         {
             UnlockedBugNet();
         }
@@ -22,11 +20,9 @@ static class AntoneQuestMods
 
     public static void UnlockedBugNet()
     {
-        GameObject prep_quest = GameObject.Find("Prep Quest");
-        Transform prep_subquests = prep_quest.transform.Find("Subquests");
-        GameObject entomologist_quest = prep_subquests.Find("Entomologist").gameObject;
+        GameObject entomologist_quest = Util.GetByPath("West (Forest)/Prep Quest/Subquests/Entomologist");
         QuestStates entomologist_quest_qs = entomologist_quest.GetComponent<QuestStates>();
-        GameObject sneak_seq = entomologist_quest.transform.Find("Sneak up sequence").gameObject;
+        GameObject sneak_seq = Util.GetByPath("West (Forest)/Prep Quest/Subquests/Entomologist/Sneak up sequence");
         entomologist_quest_qs.states[1].stateObjects.Add(sneak_seq);
         if (entomologist_quest_qs.StateID == 1)
         {
