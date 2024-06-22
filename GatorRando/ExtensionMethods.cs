edbdafb4e04ff5745d6196fed57ecade7015f750
Na <>
2024-06-22 18:38:23 -0400

    Suppress Publicizer warning for accessing private field
    It's the only way to do what we need here.
    
    NB: VSCode still seems to display a warning on the method itself? Not sure what's up with that.

--- GatorRando/ExtensionMethods.cs
+++ GatorRando/ExtensionMethods.cs
@@ -4,6 +4,11 @@ namespace GatorRando;
 
 public static class ExtensionMethods
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "Member Access",
+        "Publicizer001:Accessing a member that was not originally public",
+        Justification = "There's no public way to remove persistent listeners"
+    )]
     public static void ObliteratePersistentListenerByIndex(this UnityEvent uevent, int eventIndex)
     {
         var persistentCalls = uevent.m_PersistentCalls;
