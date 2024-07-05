7ed2b0b554930a6a84a2d682fd0ea524f0e1968e
Na <>
2024-07-05 17:48:07 -0400

    Add IL Patches for Bracelet and Junk shops

--- GatorRando/patches/BraceletShopDialoguePatch.cs
+++ GatorRando/patches/BraceletShopDialoguePatch.cs
@@ -1,76 +1,53 @@
-using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 using HarmonyLib;
-using UnityEngine;
 
 namespace GatorRando.Patches;
 
 [HarmonyPatch(typeof(BraceletShopDialogue))]
 static class BraceletShopDialoguePatch
 {
-    [HarmonyPrefix]
-    [HarmonyPatch("Interact")]
-    static bool PreInteract(BraceletShopDialogue __instance)
-    {
-        CoroutineUtil.Start(RunShopModified(__instance));
-        return false;
-    }
 
-    private static IEnumerator RunShopModified(BraceletShopDialogue bsd)
+    [HarmonyTranspiler]
+    [HarmonyPatch("RunShop", MethodType.Enumerator)]
+    static IEnumerable<CodeInstruction> TranspileRunShop(IEnumerable<CodeInstruction> instructions)
     {
-        //TODO: Revise to remove duplicated code
-        Game.DialogueDepth++;
-        bsd.state = GameData.g.ReadInt(bsd.SaveID, 0);
-        bsd.itemResource.ForceShow = true;
-        int price = bsd.price;
-        MultilingualTextDocument.SetPlaceholder(0, price.ToString("0"));
-        yield return bsd.LoadDialogue(bsd.promptDialogue);
-        bsd.itemResource.ForceShow = false;
-        if (DialogueManager.optionChosen == 1)
+        CodeInstruction nop = new(OpCodes.Nop);
+        int counter = 1;
+        foreach (var instruction in instructions)
         {
-            if (bsd.itemResource.Amount >= price)
+
+            //NB: counter is individual instruction count which is different from the IL_ labels (which are byte-based)
+            CodeInstruction newInstruction = counter switch
             {
-                bsd.itemResource.Amount -= price;
-                yield return bsd.LoadDialogue(bsd.purchaseDialogue);
-                bsd.itemResource.ForceShow = false;
-                Player.movement.Stamina = 0f;
-                ArchipelagoManager.CollectLocationByName(bsd.SaveID);
-                yield return null;
-                // Player.itemManager.Refresh();
-                // yield return bsd.DoBraceletGet(); //TODO: OVERWRITE with appropriate UI for archipelago item
-                bsd.state++;
-                GameData.g.Write(bsd.SaveID, bsd.state);
-                if (bsd.state >= bsd.braceletsInStock)
-                {
-                    bsd.actors[0].showNpcMarker = false;
-                    if (bsd.CheckIfAllBraceletShops())
-                    {
-                        Game.DialogueDepth++;
-                        yield return bsd.LoadDialogue(bsd.allPurchased);
-                        yield return bsd.StartCoroutine(bsd.Poof());
-                        yield return new WaitForSeconds(1.5f);
-                        yield return CoroutineUtil.Start(DialogueManager.d.LoadChunk(bsd.document.FetchChunk(bsd.afterAllPurchased), null, DialogueManager.DialogueBoxBackground.Standard, true, true, false, false));
-                        bsd.rewardNPC.GiveReward();
-                        Game.DialogueDepth--;
-                    }
-                    else
-                    {
-                        yield return bsd.LoadDialogue(bsd.leaveDialogue);
-                        yield return bsd.StartCoroutine(bsd.Poof());
-                    }
-                }
-            }
-            else
-            {
-                yield return bsd.LoadDialogue(bsd.notEnoughDialogue);
-            }
+                //IL_0308-IL_030E optionally noppable
+                //IL_030F-IL_0312 noppable
+                //  call instance string BraceletShopDialogue::get_SaveID()
+                //  call ArchipelagoManager.CollectLocationByName(string)
+                234 => nop, // nop the  ItemManager.i dup so our stack is clean
+                237 => new(OpCodes.Ldloc_1), // push "this"
+                238 => CodeInstruction.Call(typeof(BraceletShopDialogue), "get_SaveID"), // this.get_SaveID
+                239 => CodeInstruction.Call(typeof(ArchipelagoManager), nameof(ArchipelagoManager.CollectLocationByName), [typeof(string)]),
+                240 => new(OpCodes.Pop), // clean up the bool from CollectLocationByName
+
+                //IL_033A
+                //  Replace call to BraceletShopDialogue::DoBraceletGet() with push null
+                255 => nop, // Do not push "this" onto the stack
+                256 => new(OpCodes.Ldnull), //yield return null
+
+                //IL_0398-IL03A3
+                //  call this.CheckIfAllBraceletShops()
+                //  brfalse to IL_047B
+                289 => new(OpCodes.Ldloc_1), // "this"
+                290 => CodeInstruction.Call(typeof(BraceletShopDialogue), nameof(BraceletShopDialogue.CheckIfAllBraceletShops)), // this.CheckIfAllBraceletShops()
+                291 => nop,
+                292 => new(OpCodes.Brfalse, instruction.operand), //this is the branch
+
+                _ => instruction
+            };
+
+            counter++;
+            yield return newInstruction;
         }
-        else
-        {
-            yield return bsd.LoadDialogue(bsd.noPurchaseDialogue);
-        }
-        bsd.itemResource.ForceShow = false;
-        GameData.g.Write(bsd.SaveID, bsd.state);
-        Game.DialogueDepth--;
-        yield break;
     }
 }
--- GatorRando/patches/JunkShopPatch.cs
+++ GatorRando/patches/JunkShopPatch.cs
@@ -1,98 +1,65 @@
-using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 using HarmonyLib;
-using UnityEngine;
 
 namespace GatorRando.Patches;
 
 [HarmonyPatch(typeof(JunkShop))]
 static class JunkShopPatch
 {
-    [HarmonyPrefix]
-    [HarmonyPatch("RunShopDialogue")]
-    static bool PreRunShopDialogue(JunkShop __instance)
+    [HarmonyTranspiler]
+    [HarmonyPatch("RunShopDialogueSequence", MethodType.Enumerator)]
+    static IEnumerable<CodeInstruction> TranspileRunShopDialogueSequence(IEnumerable<CodeInstruction> instructions)
     {
-        CoroutineUtil.Start(RunShopDialogueSequenceModified(__instance));
-        return false;
+        CodeInstruction nop = new(OpCodes.Nop);
+        int counter = 1;
+        foreach (var instruction in instructions)
+        {
+
+            //NB: counter is individual instruction count which is different from the IL_ labels (which are byte-based)
+            CodeInstruction newInstruction = counter switch
+            {
+                //IL_0262 - IL_0275
+                //  ArchipelagoManager.CollectLocationByName(shopItem.item.name);
+                //  QuestMods.Junk4TrashQuestMods.HideCollectedShopLocations();
+                231 => CodeInstruction.Call(typeof(UnityEngine.Object), "get_name"),
+                232 => CodeInstruction.Call(typeof(ArchipelagoManager), nameof(ArchipelagoManager.CollectLocationByName), [typeof(string)]),
+                233 => new(OpCodes.Pop),
+                234 => CodeInstruction.Call(typeof(QuestMods.Junk4TrashQuestMods), nameof(QuestMods.Junk4TrashQuestMods.HideCollectedShopLocations)),
+                235 => nop,
+                236 => nop,
+                237 => nop,
+
+                _ => instruction
+            };
+
+            counter++;
+            yield return newInstruction;
+        }
     }
 
-    private static IEnumerator RunShopDialogueSequenceModified(JunkShop js)
+    [HarmonyTranspiler]
+    [HarmonyPatch("UpdateInventory")]
+    static IEnumerable<CodeInstruction> TranspileUpdateInventory(IEnumerable<CodeInstruction> instructions)
     {
-        //TODO: Revise to remove duplicated code
-        Game.DialogueDepth++;
-        js.itemResource.ForceShow = true;
-        GameObject[] array = js.shopStateObjects;
-        for (int i = 0; i < array.Length; i++)
+        CodeInstruction nop = new(OpCodes.Nop);
+        int counter = 1;
+        foreach (var instruction in instructions)
         {
-            array[i].SetActive(true);
-        }
-        yield return CoroutineUtil.Start(DialogueManager.d.LoadChunk(js.document.FetchChunk(js.shopIntro), js.actors, DialogueManager.DialogueBoxBackground.Standard, true, true, false, false));
-        DialogueManager.optionChosen = -1;
-        CoroutineUtil.Start(DialogueManager.d.RunDialogueOptions(js.GetChoiceList()));
-        int selectedOption = 0;
-        while (DialogueManager.optionChosen == -1)
-        {
-            int currentlySelectedIndex = DialogueOptions.CurrentlySelectedIndex;
-            for (int j = 0; j < js.cameras.Length; j++)
+
+            //NB: counter is individual instruction count which is different from the IL_ labels (which are byte-based)
+            CodeInstruction newInstruction = counter switch
             {
-                js.cameras[j].SetActive(j == currentlySelectedIndex - 1);
-            }
-            if (currentlySelectedIndex != selectedOption)
-            {
-                if (currentlySelectedIndex == 0)
-                {
-                    js.uiItemResource.ClearPrice();
-                }
-                else
-                {
-                    js.uiItemResource.SetPrice(js.shopItems[js.displayedItems[currentlySelectedIndex - 1]].cost);
-                }
-            }
-            yield return null;
+                //IL_005A - IL_0097
+                //  ignore the "!this.shopItems[k].item.IsUnlocked" check
+                53 => new(OpCodes.Pop), //IL_0083 Minimal change to bypass one condition in a && chain
+
+                _ => instruction
+            };
+
+            Plugin.LogDebug($"{instruction}");
+            counter++;
+            yield return newInstruction;
         }
-        array = js.cameras;
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i].SetActive(false);
-        }
-        if (DialogueManager.optionChosen == 0)
-        {
-            js.uiItemResource.ClearPrice();
-            js.itemResource.ForceShow = false;
-            yield return CoroutineUtil.Start(DialogueManager.d.LoadChunk(js.document.FetchChunk(js.cancelDialogue), js.actors, DialogueManager.DialogueBoxBackground.Standard, true, true, false, false));
-        }
-        else
-        {
-            int num = DialogueManager.optionChosen - 1;
-            JunkShop.ShopItem shopItem = js.shopItems[js.displayedItems[num]];
-            if (js.itemResource.Amount >= shopItem.cost)
-            {
-                js.uiItemResource.ClearPrice();
-                js.itemResource.ForceShow = false;
-                js.itemResource.Amount -= shopItem.cost;
-                ArchipelagoManager.CollectLocationByName(shopItem.item.name);
-                shopItem.isHidden = true;
-                js.shopItems[js.displayedItems[num]] = shopItem;
-                js.UpdateInventory();
-                yield return CoroutineUtil.Start(js.itemGet.RunSequence(shopItem.item.sprite, shopItem.item.DisplayName, js.document.FetchChunk(shopItem.unlockChunk), js.actors));
-            }
-            else
-            {
-                yield return CoroutineUtil.Start(DialogueManager.d.LoadChunk(js.document.FetchChunk(js.notEnoughDialogue), js.actors, DialogueManager.DialogueBoxBackground.Standard, true, true, false, false));
-                js.uiItemResource.ClearPrice();
-                js.itemResource.ForceShow = false;
-            }
-        }
-        array = js.shopStateObjects;
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i].SetActive(false);
-        }
-        if (js.displayedItemCount == 0)
-        {
-            yield return DialogueManager.d.LoadChunk(js.document.FetchChunk(js.soldOutDialogue), js.actors, DialogueManager.DialogueBoxBackground.Standard, true, true, false, false);
-            yield return js.stateMachine.ProgressState(-1);
-        }
-        Game.DialogueDepth--;
-        yield break;
     }
 }
