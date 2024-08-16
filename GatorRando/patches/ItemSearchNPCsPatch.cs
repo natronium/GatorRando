b1e2ced2078b95bd3b8b6a3a8575acb50dc63f43
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2024-08-16 16:49:19 -0500

    draft adding main quest dialogue actors to checkfinder

--- GatorRando/patches/ItemSearchNPCsPatch.cs
+++ GatorRando/patches/ItemSearchNPCsPatch.cs
@@ -1,16 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using GatorRando.UIMods;
 using HarmonyLib;
+using UnityEngine;
 
 namespace GatorRando.Patches;
 
 [HarmonyPatch(typeof(ItemSearchNPCs))]
 static class ItemSearchNPCsPatch
 {
+    private static List<DialogueActor> additionalActors;
     [HarmonyPostfix]
     [HarmonyPatch("GetList")]
     static void PostGetList(ref DialogueActor[] __result)
     {
+        if (additionalActors == null)
+        {
+            GameObject act1Quests = Util.GetByPath("/NorthWest (Tutorial Island)/Act 1/Quests/");
+            additionalActors.AddRange(act1Quests.GetComponentsInChildren<DialogueActor>());
+            GameObject coolKidsQuest = Util.GetByPath("/East (Creeklands)/Cool Kids Quest/");
+            additionalActors.AddRange(coolKidsQuest.GetComponentsInChildren<DialogueActor>());
+            GameObject prepQuest = Util.GetByPath("/West (Forest)/Prep Quest/");
+            additionalActors.AddRange(prepQuest.GetComponentsInChildren<DialogueActor>());
+            GameObject theatreQuest = Util.GetByPath("/North (Mountain)/Theatre Quest/");
+            additionalActors.AddRange(theatreQuest.GetComponentsInChildren<DialogueActor>());
+        }
+        __result =  [.. __result, .. additionalActors];
         __result = SettingsMods.GetCheckfinderBehavior() switch
         {
             SettingsMods.CheckfinderBehavior.Logic => Array.FindAll(__result, dialogueActor => ArchipelagoManager.IsLocationAccessible(dialogueActor.profile.id)),
