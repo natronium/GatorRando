2394df19d88d32bc92b71be18fe0891cb6222bc4
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2024-09-23 19:54:23 -0500

    implement logic and check filtering for main quest NPCs

--- GatorRando/ArchipelagoManager.cs
+++ GatorRando/ArchipelagoManager.cs
@@ -65,7 +65,7 @@ public static class ArchipelagoManager
     private static void UpdateAccessibleLocations() => AccessibleLocations = LocationsAccess.GetAccessibleLocations(GetObtainedItems(), GetOptions(), functions);
     public static bool IsLocationAccessible(string gatorName)
     {
-        if (excludedNPCs.Contains(gatorName))
+        if (excludedNPCs.Contains(gatorName) || IsLocationCollected(gatorName))
         {
             return false;
         }
@@ -79,14 +79,141 @@ public static class ArchipelagoManager
             return false;
         }
     }
-    
+
     public static bool IsLocationACheck(string gatorName)
     {
-        if (excludedNPCs.Contains(gatorName))
+        if (excludedNPCs.Contains(gatorName) || IsLocationCollected(gatorName))
+        {
+            return false;
+        }
+        try
+        {
+            Locations.Entries.First(entry => entry.clientNameId == gatorName);
+        }
+        catch (InvalidOperationException)
         {
             return false;
         }
         return true;
+    }
+
+    public static string ConvertDAToGatorName(DialogueActor dialogueActor)
+    {
+        string gatorName = dialogueActor.profile.id switch
+        {
+            "NPC_Tut_Dog" => DetectJillVariant(dialogueActor),
+            "NPC_Tut_Frog" => DetectAveryVariant(dialogueActor),
+            "NPC_Tut_Horse" => DetectMartinVariant(dialogueActor),
+            "NPC_Cool_Goose" when dialogueActor.transform.parent.name != "Goose Quest" => "Unhandled Duke",
+            "NPC_Cool_Boar" when dialogueActor.transform.parent.name != "Boar Quest" => "Unhandled Jada",
+            "NPC_Cool_Wolf" when dialogueActor.transform.parent.name != "Wolf Quest" => "Unhandled Lucas",
+            "NPC_Susanne" when dialogueActor.transform.parent.name != "Character" => "Unhandled Susanne",
+            "NPC_Gene" when dialogueActor.transform.parent.name != "Character" => "Unhandled Gene",
+            "NPC_Antone" when dialogueActor.transform.parent.name != "Character" => "Unhandled Antone",
+            "NPC_Theatre_Cowboy" when dialogueActor.transform.parent.name != "Cowfolk" => "Unhandled Velma",
+            "NPC_Theatre_Space" when dialogueActor.transform.parent.name != "Space!!!" => "Unhandled Andromeda",
+            "NPC_Theatre_Bat" when dialogueActor.transform.parent.name != "Vampire" => "Unhandled Esme",
+            string name => name
+        };
+        return gatorName;
+    }
+
+    private static string DetectMartinVariant(DialogueActor dialogueActor)
+    {
+        return dialogueActor.transform.parent.name switch
+        {
+            "Martin Quest" => "Tutorial Martin",
+            "Cool CoolKids" => "Cool Kid Martin",
+            _ => "Unhandled Martin"
+        };
+    }
+
+    private static string DetectAveryVariant(DialogueActor dialogueActor)
+    {
+        return dialogueActor.transform.parent.name switch
+        {
+            "Avery Quest" => "Tutorial Avery",
+            "Introduction" => "Theatre Introduction Avery",
+            "Unhappy" => "Theatre Finale Avery",
+            _ => "Unhandled Avery"
+        };
+    }
+
+    private static string DetectJillVariant(DialogueActor dialogueActor)
+    {
+        return dialogueActor.transform.parent.name switch
+        {
+            "Fantasy" => "Tutorial Stick Jill",
+            "Studying" => "Tutorial End Jill",
+            "Intro Actors" => "Prep Introduction Jill",
+            "Sad Jill" => "Prep Finale Jill",
+            _ => "Unhandled Jill"
+        };
+    }
+
+    public static bool IsMainQuestAccessible(DialogueActor dialogueActor)
+    {
+        if (IsMainQuestACheck(dialogueActor))
+        {
+            string gatorName = ConvertDAToGatorName(dialogueActor);
+            string[] checks = ChecksForNPC(gatorName);
+            return checks.Select(IsLocationAccessible).Any(b => b);
+        }
+        return false;
+    }
+
+    public static bool IsMainQuestACheck(DialogueActor dialogueActor)
+    {
+        string gatorName = ConvertDAToGatorName(dialogueActor);
+        // filter out non-main quest NPCs from actors pulled to populate the Main Quest list
+        string[] filtered_npcs = ["NPC_WannaBeHawk", "NPC_BigSis", "NPC_Destroy_Elephant", "NPC_Chess_Eagle", "NPC_Warrior_Beaver", "NPC_Obstacle_Ostrich",
+            "NPC_Theatre_Cow1", "NPC_Theatre_Cow2", "NPC_Theatre_Cow3", "NPC_FetchVulture", "NPC_SurfOpossum", "NPC_TripLizard", "Signs"];
+        if (gatorName == "" || gatorName.Contains("Unhandled") || filtered_npcs.Contains(gatorName))
+        {
+            return false;
+        }
+        string[] checks = ChecksForNPC(gatorName);
+        return checks.Select(IsLocationACheck).Any(b => b);
+    }
+
+    public static bool IsNPCACheck(DialogueActor dialogueActor)
+    {
+        string gatorName = dialogueActor.profile.id;
+        if (gatorName == "NPC_BraceletMonkey")
+        {
+            gatorName = WhichBraceletMonkey(dialogueActor);
+        }
+        string[] checks = ChecksForNPC(gatorName);
+        return checks.Select(IsLocationACheck).Any(b => b);
+    }
+
+    public static bool IsNPCAccessible(DialogueActor dialogueActor)
+    {
+        string gatorName = dialogueActor.profile.id;
+        if (gatorName == "NPC_BraceletMonkey")
+        {
+            gatorName = WhichBraceletMonkey(dialogueActor);
+        }
+        string[] checks = ChecksForNPC(gatorName);
+        return checks.Select(IsLocationAccessible).Any(b => b);
+    }
+    
+    private static string WhichBraceletMonkey(DialogueActor dialogueActor)
+    {
+        if (dialogueActor.transform.parent.parent.name == "North (Mountain)")
+            {
+                return "BraceletShop3";
+            }
+            else
+            {
+                return dialogueActor.transform.parent.parent.parent.name switch
+                {
+                    "NorthWest (Tutorial Island)" => "BraceletShop2",
+                    "East (Creeklands)" => "BraceletShop1",
+                    "West (Forest)" => "BraceletShop0",
+                    _ => "Unhandled"
+                };
+            }
     }
 
     public static bool IsLocationAccessible(PersistentObject gatorObject)
@@ -130,6 +257,63 @@ public static class ArchipelagoManager
             Plugin.LogWarn($"Tried to check accessibility of location {gatorObject.id}, which is not a Pot, Race, Chest, or a BreakableObject.");
             return false;
         }
+    }
+
+    public static string[] ChecksForNPC(string gatorName)
+    {
+        return gatorName switch
+        {
+            "NPC_TutIsland_Duck" => ["NPC_TutIsland_Duck", "Sword_Wood"],
+            "NPC_TutIsland_Bear" => ["NPC_TutIsland_Bear", "Item_Ragdoll"],
+            "NPC_TutIsland_Giraffe" => ["NPC_TutIsland_Giraffe", "Hat_Slime"],
+            "NPC_Balloon_Owl" => ["NPC_Balloon_Owl", "Item_Balloon"],
+            "NPC_FetchVulture" => ["NPC_FetchVulture", "BROKEN WHEELIE THINGY", "Shield_ScooterBoardBlue"],
+            "NPC_SurfOpossum" => ["NPC_SurfOpossum", "Shield_Tube"],
+            "NPC_Chess_Eagle" => ["NPC_Chess_Eagle", "Shield_Chessboard"],
+            "NPC_Clumsy_Jackal" => ["NPC_Clumsy_Jackal", "Sword_Pencil", "Thrown_Pencil_1", "Thrown_Pencil_2", "Thrown_Pencil_3"],
+            "NPC_Old_Man_Sloth" => ["NPC_Old_Man_Sloth", "Item_Gum"],
+            "NPC_Skate_Pug" => ["NPC_Skate_Pug", "Shield_Skateboard"],
+            "NPC_Rescue_Robin" => ["NPC_Rescue_Robin", "Shield_Palette"],
+            "NPC_Fetch_Shark" => ["NPC_Fetch_Shark", "QuestItem_Retainer"],
+            "NPC_Destroy_Rabbit" => ["NPC_Destroy_Rabbit", "Sword_Paintbrush"],
+            "NPC_Ninja_Crane" => ["NPC_Ninja_Crane", "Item_Shuriken"],
+            "NPC_Ninja_Anteater" => ["NPC_Ninja_Anteater", "Sword_Nunchucks", "Hat_Ninja"],
+            "NPC_Obstacle_Ostrich" => ["NPC_Obstacle_Ostrich", "Hat_SkateHelmet"],
+            "NPC_LunchSwapSkink" => ["NPC_LunchSwapSkink", "Hat_Princess"],
+            "NPC_Warrior_Beaver" => ["NPC_Warrior_Beaver", "Shield_TowerShield"],
+            "NPC_Shy_Cat" => ["NPC_Shy_Cat", "Item_Camera"],
+            "NPC_TutuCat" => ["NPC_TutuCat", "Sword_Wand"],
+            "NPC_BombBowlMole" => ["NPC_BombBowlMole", "Item_Bomb"],
+            "NPC_BigWhale" => ["NPC_BigWhale", "Hat_Space"],
+            "NPC_Rock_Fox" => ["NPC_Rock_Fox", "Rock"],
+            "NPC_Mistakes_Rat" => ["NPC_Mistakes_Rat", "Hat_Beret"],
+            "NPC_TripLizard" => ["NPC_TripLizard", "Sword_CBSpear"],
+            "NPC_TrashPanda" => ["NPC_TrashPanda", "Item_StickyHand", "Shield_Stretch", "Shield_TrashCanLid", "Sword_Wrench", "Item_PaintGun", "Sword_Grabby"],
+            "Tutorial Stick Jill" => ["Sword_Stick"],
+            "Tutorial End Jill" => ["NPC_TutIsland_Duck", "Sword_Wood"],
+            "Prep Introduction Jill" => ["BEACH ROCK", "Sword_RockHammer", "HALF A CHEESE SANDWICH", "Shield_Platter", "Sword_Net", "NPC_Tut_Dog"],
+            "Prep Finale Jill" => ["NPC_Tut_Dog"],
+            "NPC_Susanne" => ["BEACH ROCK", "Sword_RockHammer"],
+            "NPC_Gene" => ["HALF A CHEESE SANDWICH", "Shield_Platter"],
+            "NPC_Antone" => ["Sword_Net"],
+            "Tutorial Avery" => ["Hat_Basic", "Shirt"],
+            "Theatre Introduction Avery" => ["NPC_Tut_Frog", "Item_SpaceGun", "Sword_Laser", "Hat_Western", "ICE CREAM", "Hat_Vampire", "NPC_Part-Timer"],
+            "Theatre Finale Avery" => ["NPC_Tut_Frog"],
+            "NPC_Theatre_Cowboy" => ["Hat_Western"],
+            "NPC_Theatre_Space" => ["Item_SpaceGun", "Sword_Laser"],
+            "NPC_Theatre_Bat" => ["ICE CREAM", "Hat_Vampire", "NPC_Part-Timer"],
+            "NPC_Part-Timer" => ["ICE CREAM"],
+            "Tutorial Martin" => ["POT?", "Shield_PotLid"],
+            "Cool Kid Martin" => ["NPC_Tut_Horse", "Shield_Martin", "Hat_DetectiveCowl", "CLIPPINGS", "BUCKET", "WATER", "Shield_Leaf"],
+            "NPC_Cool_Goose" => ["Hat_DetectiveCowl"],
+            "NPC_Cool_Boar" => ["CLIPPINGS", "BUCKET", "WATER", "Shield_Leaf"],
+            "NPC_Cool_Wolf" => ["Shield_Martin"],
+            "BraceletShop0" => ["BraceletShop0", "NPC_BraceletMonkey"],
+            "BraceletShop1" => ["BraceletShop1", "NPC_BraceletMonkey"],
+            "BraceletShop2" => ["BraceletShop2", "NPC_BraceletMonkey"],
+            "BraceletShop3" => ["BraceletShop3", "NPC_BraceletMonkey"],
+            _ => [gatorName]
+        };
     }
 
     static List<string> ItemsInItemGroup(Items.ItemGroup itemGroup) =>
--- GatorRando/patches/ItemSearchNPCsPatch.cs
+++ GatorRando/patches/ItemSearchNPCsPatch.cs
@@ -18,21 +18,54 @@ static class ItemSearchNPCsPatch
         if (additionalActors == null)
         {
             GameObject act1Quests = Util.GetByPath("/NorthWest (Tutorial Island)/Act 1/Quests/");
-            additionalActors.AddRange(act1Quests.GetComponentsInChildren<DialogueActor>());
+            additionalActors = new List<DialogueActor>(act1Quests.GetComponentsInChildren<DialogueActor>(true));
             GameObject coolKidsQuest = Util.GetByPath("/East (Creeklands)/Cool Kids Quest/");
-            additionalActors.AddRange(coolKidsQuest.GetComponentsInChildren<DialogueActor>());
+            additionalActors.AddRange(coolKidsQuest.GetComponentsInChildren<DialogueActor>(true));
             GameObject prepQuest = Util.GetByPath("/West (Forest)/Prep Quest/");
-            additionalActors.AddRange(prepQuest.GetComponentsInChildren<DialogueActor>());
+            additionalActors.AddRange(prepQuest.GetComponentsInChildren<DialogueActor>(true));
             GameObject theatreQuest = Util.GetByPath("/North (Mountain)/Theatre Quest/");
-            additionalActors.AddRange(theatreQuest.GetComponentsInChildren<DialogueActor>());
+            additionalActors.AddRange(theatreQuest.GetComponentsInChildren<DialogueActor>(true));
+            additionalActors = additionalActors.FindAll(dialogueActor => dialogueActor.profile);
+            //TODO: remove extraneous additional actors like signs, and possibly main quest actors with finished (sub)quests? 
         }
-        __result =  [.. __result, .. additionalActors];
-        __result = SettingsMods.GetCheckfinderBehavior() switch
+
+        List<DialogueActor> filteredMainActors = SettingsMods.GetCheckfinderBehavior() switch
         {
-            SettingsMods.CheckfinderBehavior.Logic => Array.FindAll(__result, dialogueActor => ArchipelagoManager.IsLocationAccessible(dialogueActor.profile.id)),
-            SettingsMods.CheckfinderBehavior.ChecksOnly => Array.FindAll(__result, dialogueActor => ArchipelagoManager.IsLocationACheck(dialogueActor.profile.id)),
+            SettingsMods.CheckfinderBehavior.Logic => additionalActors.FindAll(ArchipelagoManager.IsMainQuestAccessible),
+            SettingsMods.CheckfinderBehavior.ChecksOnly => additionalActors.FindAll(ArchipelagoManager.IsMainQuestACheck),
+            SettingsMods.CheckfinderBehavior.Original => additionalActors,
+            _ => throw new Exception("Invalid enum value for CheckfinderBehavior"),
+        };
+
+        DialogueActor[] sideQuestActors = SettingsMods.GetCheckfinderBehavior() switch
+        {
+            SettingsMods.CheckfinderBehavior.Logic => Array.FindAll(__result, ArchipelagoManager.IsNPCAccessible),
+            SettingsMods.CheckfinderBehavior.ChecksOnly => Array.FindAll(__result, ArchipelagoManager.IsNPCACheck),
             SettingsMods.CheckfinderBehavior.Original => __result,
             _ => throw new Exception("Invalid enum value for CheckfinderBehavior"),
+        };
+
+        __result =  [.. sideQuestActors, .. filteredMainActors];
+
+        foreach(DialogueActor actor in __result){
+            Plugin.LogInfo("ID:" + actor.profile.id + ", Parent:" + actor.transform.parent.name);
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch("IsValid")]
+    static void PostIsValid(DialogueActor item, ref bool __result)
+    {
+        __result = __result && item.gameObject.activeInHierarchy;
+        string gatorName = ArchipelagoManager.ConvertDAToGatorName(item);
+        string[] checks = ArchipelagoManager.ChecksForNPC(gatorName);
+        __result = gatorName switch
+        {
+            "Tutorial Avery" => !checks.Select(ArchipelagoManager.IsLocationCollected).All(b => b),
+            "Tutorial Stick Jill" => !checks.Select(ArchipelagoManager.IsLocationCollected).All(b => b),
+            "Tutorial End Jill" => !checks.Select(ArchipelagoManager.IsLocationCollected).All(b => b),
+            "Tutorial Martin" => !checks.Select(ArchipelagoManager.IsLocationCollected).All(b => b),
+            _ => __result,
         };
     }
 }
