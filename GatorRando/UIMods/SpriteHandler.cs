14526468d1f2343b158a5ebb0ab0321568f3a417
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2025-07-06 16:41:08 -0500

    scaffold out loading sprites from all three sources (new, asset, those not loadable from asset)

--- GatorRando/UIMods/SpriteHandler.cs
+++ GatorRando/UIMods/SpriteHandler.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using System.Linq;
+using System;
 
 namespace GatorRando.UIMods;
 
@@ -11,70 +12,73 @@ public static class SpriteHandler
 {
     private static Sprite[] builtinSprites;
     private static readonly List<Sprite> newSprites = [];
+    private static readonly Dictionary<string, Sprite> loadedAssetSprites = [];
+    private static readonly Dictionary<string, Sprite> duplicatedSprites = [];
     private static readonly Dictionary<string, string> existingSpritePaths = new()
     {
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_core_bracelet_blue.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_core_bracelet_magenta.png"},
+        {"Bracelet", "Assets/UI/Images/Item Sprites/Itemsprite_core_bracelet_blue.png"},
+        {"Bracelet Magenta", "Assets/UI/Images/Item Sprites/Itemsprite_core_bracelet_magenta.png"},
         {"Craft Stuff", "Assets/UI/Images/Item Sprites/Itemsprite_core_crafting.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_core_purple.png"},
+        {"Bracelet Purple", "Assets/UI/Images/Item Sprites/Itemsprite_core_purple.png"},
         {"Shirt", "Assets/UI/Images/Item Sprites/Itemsprite_core_shirt.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_core_yellow.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_basic.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_beret.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_bucket.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_cowboy.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_detective.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_frills.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_ninja.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_princess.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_skate.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_slime.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_spacehelmet.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_hat_vampire.png"},
+        {"Bracelet Yellow", "Assets/UI/Images/Item Sprites/Itemsprite_core_yellow.png"},
+        {"Hat_Basic", "Assets/UI/Images/Item Sprites/Itemsprite_hat_basic.png"},
+        {"Hat_Beret", "Assets/UI/Images/Item Sprites/Itemsprite_hat_beret.png"},
+        {"Hat_Bucket", "Assets/UI/Images/Item Sprites/Itemsprite_hat_bucket.png"},
+        {"Hat_Western", "Assets/UI/Images/Item Sprites/Itemsprite_hat_cowboy.png"},
+        {"Hat_DetectiveCowl", "Assets/UI/Images/Item Sprites/Itemsprite_hat_detective.png"},
+        {"Hat_Frills", "Assets/UI/Images/Item Sprites/Itemsprite_hat_frills.png"},
+        {"Hat_Ninja", "Assets/UI/Images/Item Sprites/Itemsprite_hat_ninja.png"},
+        {"Hat_Princess", "Assets/UI/Images/Item Sprites/Itemsprite_hat_princess.png"},
+        {"Hat_SkateHelmet", "Assets/UI/Images/Item Sprites/Itemsprite_hat_skate.png"},
+        {"Hat_Slime", "Assets/UI/Images/Item Sprites/Itemsprite_hat_slime.png"},
+        {"Hat_Space", "Assets/UI/Images/Item Sprites/Itemsprite_hat_spacehelmet.png"},
+        {"Hat_Vampire", "Assets/UI/Images/Item Sprites/Itemsprite_hat_vampire.png"},
         {"POT?", "Assets/UI/Images/Item Sprites/Itemsprite_quest_pot.png"},
         {"QuestItem_Retainer", "Assets/UI/Images/Item Sprites/Itemsprite_quest_retainer.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_bigleaf.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_chessboard.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_innertube.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_martin.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_palette.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_platter.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_potlid.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_scooterboardblue.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_scooterboardgreen.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_skateboard.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_tower.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_trampoline.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_shield_trashcanlid.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_balloon.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_bowlingbomb.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_camera.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_gum.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_megaphone.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_paintblaster.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_paperstars.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_ragdoll.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_rock.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_spaceblaster.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_stickyhand.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_special_textjill.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_sword_cavemanhammer.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_sword_grabber.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_sword_lasersword.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_sword_net.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_sword_nunchucks.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_sword_paintbrush.png"},
-        {"Thrown_Pencil", "Assets/UI/Images/Item Sprites/Itemsprite_sword_pencil.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_sword_princesswand.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_sword_spear.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_sword_stick.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_sword_wooden.png"},
-        // {"ohai", "Assets/UI/Images/Item Sprites/Itemsprite_sword_wrench.png"},
+        {"Shield_Leaf", "Assets/UI/Images/Item Sprites/Itemsprite_shield_bigleaf.png"},
+        {"Shield_Chessboard", "Assets/UI/Images/Item Sprites/Itemsprite_shield_chessboard.png"},
+        {"Shield_Tube", "Assets/UI/Images/Item Sprites/Itemsprite_shield_innertube.png"},
+        {"Shield_Martin", "Assets/UI/Images/Item Sprites/Itemsprite_shield_martin.png"},
+        {"Shield_Palette", "Assets/UI/Images/Item Sprites/Itemsprite_shield_palette.png"},
+        {"Shield_Platter", "Assets/UI/Images/Item Sprites/Itemsprite_shield_platter.png"},
+        {"Shield_PotLid", "Assets/UI/Images/Item Sprites/Itemsprite_shield_potlid.png"},
+        {"Shield_ScooterBoardBlue", "Assets/UI/Images/Item Sprites/Itemsprite_shield_scooterboardblue.png"},
+        {"Shield_ScooterBoardGreen", "Assets/UI/Images/Item Sprites/Itemsprite_shield_scooterboardgreen.png"},
+        {"Shield_Skateboard", "Assets/UI/Images/Item Sprites/Itemsprite_shield_skateboard.png"},
+        {"Shield_TowerShield", "Assets/UI/Images/Item Sprites/Itemsprite_shield_tower.png"},
+        {"Shield_Stretch", "Assets/UI/Images/Item Sprites/Itemsprite_shield_trampoline.png"},
+        {"Shield_TrashCanLid", "Assets/UI/Images/Item Sprites/Itemsprite_shield_trashcanlid.png"},
+        {"Item_Balloon", "Assets/UI/Images/Item Sprites/Itemsprite_special_balloon.png"},
+        {"Item_Bomb", "Assets/UI/Images/Item Sprites/Itemsprite_special_bowlingbomb.png"},
+        {"Item_Camera", "Assets/UI/Images/Item Sprites/Itemsprite_special_camera.png"},
+        {"Item_Gum", "Assets/UI/Images/Item Sprites/Itemsprite_special_gum.png"},
+        {"Item_SearchNPCs", "Assets/UI/Images/Item Sprites/Itemsprite_special_megaphone.png"},
+        {"Item_PaintGun", "Assets/UI/Images/Item Sprites/Itemsprite_special_paintblaster.png"},
+        {"Item_Shuriken", "Assets/UI/Images/Item Sprites/Itemsprite_special_paperstars.png"},
+        {"Item_Ragdoll", "Assets/UI/Images/Item Sprites/Itemsprite_special_ragdoll.png"},
+        {"Rock", "Assets/UI/Images/Item Sprites/Itemsprite_special_rock.png"},
+        {"Item_SpaceGun", "Assets/UI/Images/Item Sprites/Itemsprite_special_spaceblaster.png"},
+        {"Item_StickyHand", "Assets/UI/Images/Item Sprites/Itemsprite_special_stickyhand.png"},
+        {"Item_SearchObjects", "Assets/UI/Images/Item Sprites/Itemsprite_special_textjill.png"},
+        {"Sword_RockHammer", "Assets/UI/Images/Item Sprites/Itemsprite_sword_cavemanhammer.png"},
+        {"Sword_Grabby", "Assets/UI/Images/Item Sprites/Itemsprite_sword_grabber.png"},
+        {"Sword_Laser", "Assets/UI/Images/Item Sprites/Itemsprite_sword_lasersword.png"},
+        {"Sword_Net", "Assets/UI/Images/Item Sprites/Itemsprite_sword_net.png"},
+        {"Sword_Nunchucks", "Assets/UI/Images/Item Sprites/Itemsprite_sword_nunchucks.png"},
+        {"Sword_Paintbrush", "Assets/UI/Images/Item Sprites/Itemsprite_sword_paintbrush.png"},
+        {"Sword_Pencil", "Assets/UI/Images/Item Sprites/Itemsprite_sword_pencil.png"},
+        {"Sword_Wand", "Assets/UI/Images/Item Sprites/Itemsprite_sword_princesswand.png"},
+        {"Sword_CBSpear", "Assets/UI/Images/Item Sprites/Itemsprite_sword_spear.png"},
+        {"Sword_Stick", "Assets/UI/Images/Item Sprites/Itemsprite_sword_stick.png"},
+        {"Sword_Wood", "Assets/UI/Images/Item Sprites/Itemsprite_sword_wooden.png"},
+        {"Sword_Wrench", "Assets/UI/Images/Item Sprites/Itemsprite_sword_wrench.png"},
 
         // LOOKING FOR GatorMewhenyouaremyfriend
         // Itemsprite_quest_icecream
         // Itemsprite_quest_clippings
         // Itemsprite_quest_bucketfull
+        // Magic Ore
     };
     private static readonly Dictionary<string, AddedTexture> newSpriteInformation = new()
     {
@@ -88,6 +92,15 @@ public static class SpriteHandler
         {"Archipelago", new AddedTexture("GatorRando.Sprites.archipelago_sticker_style.png", 200, 200)},
         {"Flag", new AddedTexture("GatorRando.Sprites.checkered_flag.png", 200, 200)},
         {"Socks", new AddedTexture("GatorRando.Sprites.giant_socks.png", 200, 200)},
+    };
+
+    private static readonly Dictionary<string, string> spritesToDuplicateInformation = new()
+    {
+        {"Friend", "GatorMewhenyouaremyfriend"},
+        {"ICE CREAM", "Itemsprite_quest_icecream"},
+        {"CLIPPINGS", "Itemsprite_quest_clippings"},
+        {"WATER", "Itemsprite_quest_bucketfull"},
+        {"BEACH ROCK", "Itemsprite_quest_clippings"},
     };
 
     public readonly struct AddedTexture(string path, int width, int height)
@@ -109,8 +122,48 @@ public static class SpriteHandler
         }
         return texture2D;
     }
-    
-    //TODO Retrieve Sprites in a way that does not result in them becoming unloaded
+
+    public static void LoadSprites()
+    {
+        LoadAssetSprites();
+        DuplicateNonAssetLoadSprites();
+    }
+
+    private static void LoadAssetSprites()
+    {
+        foreach (string spriteName in existingSpritePaths.Keys)
+        {
+            loadedAssetSprites[spriteName] = Addressables.LoadAssetAsync<Sprite>(existingSpritePaths[spriteName]).WaitForCompletion();
+        }
+    }
+
+    private static void DuplicateNonAssetLoadSprites()
+    {
+        builtinSprites ??= Resources.FindObjectsOfTypeAll<Sprite>();
+        foreach (string itemName in spritesToDuplicateInformation.Keys)
+        {
+            Sprite existingSprite;
+            try
+            {
+                existingSprite = builtinSprites.First(sprite => sprite.name == spritesToDuplicateInformation[itemName]);
+            }
+            catch (InvalidOperationException)
+            {
+                Plugin.LogWarn("No sprite found, storing placeholder!");
+                existingSprite = Util.FindItemObjectByName("Placeholder").sprite;
+            }
+
+            duplicatedSprites[itemName] = DuplicateSprite(existingSprite.texture);
+        }
+    }
+
+    private static Sprite DuplicateSprite(Texture2D originalTexture)
+    {
+        Texture2D copyTexture = new(originalTexture.width, originalTexture.height);
+        copyTexture.LoadRawTextureData(originalTexture.GetRawTextureData());
+        return Sprite.Create(copyTexture, new Rect(0, 0, originalTexture.width, originalTexture.height), new Vector2(0.5f, 0.5f));
+    }
+
     public static Sprite GetSpriteForItem(string name)
     {
         // Plugin.LogDebug($"Looking for sprite for {name}");
@@ -137,28 +190,25 @@ public static class SpriteHandler
             {
                 name = "Craft Stuff";
             }
-            // if (existingSpritePaths.ContainsKey(name))
-            // {
-            //     return Addressables.LoadAssetAsync<Sprite>(existingSpritePaths[name]);
-            // } 
-
-            // Plugin.LogDebug($"Loading sprite from game itself");
-            ItemObject itemObject = Util.FindItemObjectByName(name);
-            if (itemObject != null)
+            else if (name.Contains("Friend"))
             {
-                return itemObject.sprite;
+                name = "Friend";
+            }
+            else if (name.Contains("Thrown_Pencil"))
+            {
+                name = "Sword_Pencil";
+            }
+
+            if (spritesToDuplicateInformation.ContainsKey(name))
+            {
+                return duplicatedSprites[name];
+            }
+            else if (existingSpritePaths.ContainsKey(name))
+            {
+                return loadedAssetSprites[name];
             }
             else
             {
-                builtinSprites ??= Resources.FindObjectsOfTypeAll<Sprite>();
-                if (name.Contains("Craft Stuff"))
-                {
-                    return builtinSprites.First(sprite => sprite.name == "Itemsprite_core_crafting");
-                }
-                else if (name.Contains("Friend"))
-                {
-                    return builtinSprites.First(sprite => sprite.name == "GatorMewhenyouaremyfriend");
-                }
                 Plugin.LogWarn("No sprite found, using placeholder!");
                 return Util.FindItemObjectByName("Placeholder").sprite; // Should not appear
             }
