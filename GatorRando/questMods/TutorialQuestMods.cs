d79b03006a4d50a9b8f12d5d33acf32f56db131f
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2025-07-19 08:35:53 -0500

    fix start with freeplay when not skipping prologue

--- GatorRando/questMods/TutorialQuestMods.cs
+++ GatorRando/questMods/TutorialQuestMods.cs
@@ -4,23 +4,35 @@ namespace GatorRando.QuestMods;
 
 static class TutorialQuestMods
 {
-
-    public static void StartWithFreeplay()
+    public static void QueueStartWithFreeplay()
     {
-        ReenableTutorialQuests();
         GameObject act1 = Util.GetByPath("NorthWest (Tutorial Island)/Act 1");
         QuestStates act1QuestStates = act1.GetComponent<QuestStates>();
-        act1QuestStates.states[0].onDeactivate.RemoveListener(AdvanceToEndOfTutorial);
-        act1QuestStates.states[0].onDeactivate.AddListener(AdvanceToEndOfTutorial);
-        if (act1QuestStates.StateID < 2)
+        act1QuestStates.onStateChange.RemoveListener(Act1QuestHandler);
+        act1QuestStates.onStateChange.AddListener(Act1QuestHandler);
+        if (act1QuestStates.StateID == 1)
         {
             AdvanceToEndOfTutorial();
         }
-        act1QuestStates.states[3].onActivate.RemoveListener(ReenableTutorialQuests);
-        act1QuestStates.states[3].onActivate.AddListener(ReenableTutorialQuests);
-        GameObject manager = Util.GetByPath("Managers");
-        Game game = manager.GetComponent<Game>();
-        game.SetToStory();
+        if (act1QuestStates.StateID >= 2)
+        {
+            ReenableTutorialQuests();
+        }
+    }
+
+    private static void Act1QuestHandler(int stateID)
+    {
+        switch (stateID)
+        {
+            case 0:
+                break; // Do nothing on initial cutscene
+            case 1:
+                AdvanceToEndOfTutorial(); break;
+            case 2:
+                ReenableTutorialQuests(); EnableFriendsInCutscene(); break;
+            case 3:
+                ReenableTutorialQuests(); break;
+        }
     }
 
     private static void AdvanceToEndOfTutorial()
@@ -29,10 +41,18 @@ static class TutorialQuestMods
         QuestStates act1QuestStates = act1.GetComponent<QuestStates>();
         if (act1QuestStates.StateID < 2)
         {
-            act1QuestStates.ProgressState(2);
+            act1QuestStates.SetState(2);
+            EnableFriendsInCutscene();
             ReenableTutorialQuests();
         }
-        act1QuestStates.states[2].stateObjects[0].SetActive(true); // Reenable End object to try to fix Cutscene in Start With Freeplay = true
+    }
+
+    private static void EnableFriendsInCutscene()
+    {
+        // Reenable End object to try to fix Cutscene in Start With Freeplay = true
+        GameObject act1 = Util.GetByPath("NorthWest (Tutorial Island)/Act 1");
+        QuestStates act1QuestStates = act1.GetComponent<QuestStates>();
+        act1QuestStates.states[2].stateObjects[0].SetActive(true);
     }
 
     private static void ReenableTutorialQuests()
