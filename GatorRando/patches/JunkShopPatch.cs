d756fed6bfe04e48bf4ac08f7d4c8e55da70b264
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2026-05-24 15:19:42 -0500

    update JunkShop IL Patch to reflect changes to vanilla code

--- GatorRando/patches/JunkShopPatch.cs
+++ GatorRando/patches/JunkShopPatch.cs
@@ -15,20 +15,20 @@ internal static class JunkShopPatch
     static readonly Dictionary<string, Sprite> replacementSprites = [];
     static readonly Dictionary<string, string> replacementDisplayNames = [];
     static readonly Dictionary<string, DialogueChunk> replacementDialogues = [];
-	private static Sprite GetSprite(string name) => replacementSprites[name];
-	private static string GetDisplayName(string name) => replacementDisplayNames[name];
-	private static DialogueChunk GetDialogueChunk(string name) => replacementDialogues[name];
+    private static Sprite GetSprite(string name) => replacementSprites[name];
+    private static string GetDisplayName(string name) => replacementDisplayNames[name];
+    private static DialogueChunk GetDialogueChunk(string name) => replacementDialogues[name];
 
     [HarmonyPostfix]
     [HarmonyPatch(nameof(JunkShop.Start))]
-	private static void PostStart()
+    private static void PostStart()
     {
         Junk4TrashQuestMods.HideCollectedShopLocations();
     }
 
     [HarmonyPrefix]
     [HarmonyPatch(nameof(JunkShop.RunShopDialogueSequence))]
-	private static void PreRunShopDialogueSequence(JunkShop __instance)
+    private static void PreRunShopDialogueSequence(JunkShop __instance)
     {
         if (replacementSprites.Keys.Count == 0)
         {
@@ -43,7 +43,7 @@ internal static class JunkShopPatch
                 }
                 else
                 {
-                    replacementDialogues.Add(shopItem.item.name, DialogueModifier.AddNewDialogueChunk( $"I bought {DialogueModifier.GetItemNameForItemAtLocation(itemAtLocation)}", __instance.document));
+                    replacementDialogues.Add(shopItem.item.name, DialogueModifier.AddNewDialogueChunk($"I bought {DialogueModifier.GetItemNameForItemAtLocation(itemAtLocation)}", __instance.document));
                 }
             }
         }
@@ -51,7 +51,7 @@ internal static class JunkShopPatch
 
     [HarmonyTranspiler]
     [HarmonyPatch(nameof(JunkShop.RunShopDialogueSequence), MethodType.Enumerator)]
-	private static IEnumerable<CodeInstruction> TranspileRunShopDialogueSequence(IEnumerable<CodeInstruction> instructions)
+    private static IEnumerable<CodeInstruction> TranspileRunShopDialogueSequence(IEnumerable<CodeInstruction> instructions)
     {
         CodeInstruction nop = new(OpCodes.Nop);
         int counter = 1;
@@ -73,34 +73,33 @@ internal static class JunkShopPatch
 
         foreach (CodeInstruction instruction in instructions)
         {
-
             //NB: counter is individual instruction count which is different from the IL_ labels (which are byte-based)
             CodeInstruction newInstruction = counter switch
             {
                 //IL_0262 - IL_0275
                 //  ArchipelagoManager.CollectLocationByName(shopItem.item.name);
                 //  QuestMods.Junk4TrashQuestMods.HideCollectedShopLocations();
-                231 => CodeInstruction.Call(typeof(UnityEngine.Object), "get_name"),
-                232 => CodeInstruction.Call(typeof(LocationHandling), nameof(LocationHandling.CollectLocationByName), [typeof(string)]),
-                233 => new(OpCodes.Pop),
-                234 => CodeInstruction.Call(typeof(QuestMods.Junk4TrashQuestMods), nameof(QuestMods.Junk4TrashQuestMods.HideCollectedShopLocations)),
-                235 => nop,
-                236 => nop,
-                237 => nop,
-                // 238 and 239 intentionally untouched (UpdateInventory)
-                
+                234 => CodeInstruction.Call(typeof(UnityEngine.Object), "get_name"),
+                235 => CodeInstruction.Call(typeof(LocationHandling), nameof(LocationHandling.CollectLocationByName), [typeof(string)]),
+                236 => new(OpCodes.Pop),
+                237 => CodeInstruction.Call(typeof(QuestMods.Junk4TrashQuestMods), nameof(QuestMods.Junk4TrashQuestMods.HideCollectedShopLocations)),
+                238 => nop,
+                239 => nop,
+                240 => nop,
+                // 241 and 242 intentionally untouched (UpdateInventory)
+
                 // IL_028E - IL_02AC
                 // changing the argument of CoroutineUtil.Start  to:
                 //  RunSequence(JunkShopPatch.getSprite(shopItem.item.name), JunkShopPatch.getDisplayName(shopItem.item.name), JunkShopPatch.getReplacementDialogueChunk(shopItem.item.name), this.actors)
-                245 => nop,
-                246 => nop,
-                247 => nop,
                 248 => nop,
                 249 => nop,
                 250 => nop,
                 251 => nop,
                 252 => nop,
                 253 => nop,
+                254 => nop,
+                255 => nop,
+                256 => nop,
 
 
                 //instead of RunSequence(shopItem.item.sprite, shopItem.item.DisplayName, this.document.FetchChunk(shopItem.unlockChunk), this.actors)
@@ -109,7 +108,8 @@ internal static class JunkShopPatch
 
                 _ => instruction
             };
-            if (counter == 245){
+            if (counter == 248)
+            {
                 foreach (CodeInstruction instr in secondPatch)
                 {
                     yield return instr;
@@ -122,7 +122,7 @@ internal static class JunkShopPatch
 
     [HarmonyTranspiler]
     [HarmonyPatch(nameof(JunkShop.UpdateInventory))]
-	private static IEnumerable<CodeInstruction> TranspileUpdateInventory(IEnumerable<CodeInstruction> instructions)
+    private static IEnumerable<CodeInstruction> TranspileUpdateInventory(IEnumerable<CodeInstruction> instructions)
     {
         // CodeInstruction nop = new(OpCodes.Nop);
         int counter = 1;
@@ -146,7 +146,7 @@ internal static class JunkShopPatch
 
     [HarmonyPrefix]
     [HarmonyPatch(nameof(JunkShop.GetChoiceList))]
-	private static bool PreGetChoiceList(JunkShop __instance, ref string[] __result)
+    private static bool PreGetChoiceList(JunkShop __instance, ref string[] __result)
     {
         __result = new string[__instance.displayedItemCount + 1];
         __result[0] = __instance.document.FetchString(__instance.cancelChoice, Language.Auto);
